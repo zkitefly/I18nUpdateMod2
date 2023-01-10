54ed6c24c864aba794ac5c5ce42875901633280a
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-01-10 12:43:59 +0800

    1.16.5 同步更改

--- fabric/src/main/java/com/github/tartaricacid/i18nupdatemod/mixin/MixinBookContents.java
+++ fabric/src/main/java/com/github/tartaricacid/i18nupdatemod/mixin/MixinBookContents.java
@@ -1,6 +1,3 @@
-/*
-Credit: https://github.com/kappa-maintainer/PRP
-*/
 package com.github.tartaricacid.i18nupdatemod.mixin;
 
 import com.github.tartaricacid.i18nupdatemod.I18nUpdateMod;
@@ -10,22 +7,19 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import vazkii.patchouli.client.book.BookContentExternalLoader;
-import vazkii.patchouli.common.book.Book;
+import vazkii.patchouli.client.book.BookContents;
 
-import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 
-@Mixin(BookContentExternalLoader.class)
-public class MixinContentExternalLoader {
+@Mixin(BookContents.class)
+public class MixinBookContents {
     @Inject(at = @At("HEAD"), method = "loadJson", cancellable = true, remap = false)
-    private void loadJson(Book book, Identifier resloc, @Nullable Identifier fallback, CallbackInfoReturnable<InputStream> cir) {
-        I18nUpdateMod.LOGGER.debug("loading json from {}.", resloc);
+    private void loadJson(Identifier resloc, Identifier fallback, CallbackInfoReturnable<InputStream> cir) {
+        I18nUpdateMod.LOGGER.debug("[Patchouli] loading json from {}.", resloc);
         try {
             cir.setReturnValue(MinecraftClient.getInstance().getResourceManager().getResource(resloc).getInputStream());
-        } catch (IOException e) {
-            //no-op
+        } catch (IOException ignore) {
         }
     }
 }
--- forge/src/main/java/com/github/tartaricacid/i18nupdatemod/mixin/MixinBookContents.java
+++ forge/src/main/java/com/github/tartaricacid/i18nupdatemod/mixin/MixinBookContents.java
@@ -1,31 +1,25 @@
-/*
-Credit: https://github.com/kappa-maintainer/PRP
-*/
 package com.github.tartaricacid.i18nupdatemod.mixin;
 
+import com.github.tartaricacid.i18nupdatemod.I18nUpdateMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import com.github.tartaricacid.i18nupdatemod.I18nUpdateMod;
-import vazkii.patchouli.client.book.BookContentClasspathLoader;
-import vazkii.patchouli.common.book.Book;
+import vazkii.patchouli.client.book.BookContents;
 
-import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 
-@Mixin(BookContentClasspathLoader.class)
-public class MixinBookContentClasspathLoader {
+@Mixin(BookContents.class)
+public class MixinBookContents {
     @Inject(at = @At("HEAD"), method = "loadJson", cancellable = true, remap = false)
-    private void loadJson(Book book, ResourceLocation resloc, @Nullable ResourceLocation fallback, CallbackInfoReturnable<InputStream> cir) {
+    private void loadJson(ResourceLocation resloc, ResourceLocation fallback, CallbackInfoReturnable<InputStream> cir) {
         I18nUpdateMod.LOGGER.debug("[Patchouli] loading json from {}.", resloc);
         try {
             cir.setReturnValue(Minecraft.getInstance().getResourceManager().getResource(resloc).getInputStream());
-        } catch (IOException e) {
-            //no-op
+        } catch (IOException ignore) {
         }
     }
 }
