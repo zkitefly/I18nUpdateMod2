af5a6d362a70b53478dbe8e30783f3cfe34d695b
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-01-11 13:16:34 +0800

    添加config

---
+++ fabric/src/main/java/com/github/tartaricacid/i18nupdatemod/I18nModConfig.java
@@ -0,0 +1,11 @@
+package com.github.tartaricacid.i18nupdatemod;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+
+@Config(name = I18nUpdateMod.MOD_ID)
+public class I18nModConfig implements ConfigData {
+    public static String downloadLink = "https://ghproxy.com/https://raw.githubusercontent.com/zkitefly/TranslationPackMirror/main/files";
+    public static String resourcePackName = "Minecraft-Mod-Language-Modpack-1-16-Fabric";
+    public static String md5Name = "1.16-fabric";
+}
