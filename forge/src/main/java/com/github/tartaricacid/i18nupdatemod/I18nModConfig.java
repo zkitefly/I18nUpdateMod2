806a9aceb64e89c6d69039b0e6ae8d51adc56f1b
zkitefly <64117916+zkitefly@users.noreply.github.com>
2023-01-14 17:25:53 +0800

    fix

--- forge/src/main/java/com/github/tartaricacid/i18nupdatemod/I18nModConfig.java
+++ forge/src/main/java/com/github/tartaricacid/i18nupdatemod/I18nModConfig.java
@@ -5,7 +5,7 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = I18nUpdateMod.MOD_ID)
 public class I18nModConfig implements ConfigData {
-    public static String downloadLink = "https://gitcode.net/chearlai/TranslationPackMirror/-/raw/main/files";
+    public static String downloadLink = "https://gitcode.net/chearlai/translationpackconvert/-/raw/main/files";
     public static String resourcePackName = "Minecraft-Mod-Language-Modpack-1-16T1-17";
     public static String md5Name = "1.16t1.17";
 }
