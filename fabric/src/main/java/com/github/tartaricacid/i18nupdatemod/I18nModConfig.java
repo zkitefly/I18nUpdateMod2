ee31e3ba4b51d60c52acc4846c5201b26b990372
zkitefly <64117916+zkitefly@users.noreply.github.com>
2023-01-14 17:18:33 +0800

    修改downloadLink（1.17.x-all/dev）

--- fabric/src/main/java/com/github/tartaricacid/i18nupdatemod/I18nModConfig.java
+++ fabric/src/main/java/com/github/tartaricacid/i18nupdatemod/I18nModConfig.java
@@ -5,7 +5,7 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = I18nUpdateMod.MOD_ID)
 public class I18nModConfig implements ConfigData {
-    public static String downloadLink = "https://ghproxy.com/https://raw.githubusercontent.com/zkitefly/TranslationPackConvert/main/files";
+    public static String downloadLink = "https://gitcode.net/chearlai/TranslationPackMirror/-/raw/main/files";
     public static String resourcePackName = "Minecraft-Mod-Language-Modpack-1-16-FabricT1-17-Fabric";
     public static String md5Name = "1.16-fabric";
 }
